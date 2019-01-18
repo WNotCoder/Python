1be7c1eed1b63cff07667e521da7655efce9b189
AJC <fish_wish@163.com>
2019-01-18 17:58:44 +0800

    可用，但是tomd好像对复杂html支持不太好

--- Html2MD/html2md.py
+++ Html2MD/html2md.py
@@ -1,37 +1,41 @@
 # -*- coding: utf-8 -*-
 import codecs
-import sys
+import os
 
 import tomd
 
-reload(sys)
-#sys.setdefaultencoding('utf8')  # 设置默认编码格式为'utf-8'
+load_file_path = "D:\\TEMP\\ori"
 
-save_file = 'D:\\TEMP\\md'
+save_file_path = "D:\\TEMP\\md"
 
+def getHtml(load_file_path):
+    f = open(load_file_path,'r',encoding='utf-8')
+    filename = os.path.basename(load_file_path).split('.')[0]
+    print('正在读取：' + str(filename))
+    return f,filename
 
-def run():
-    html = getHtml()
-    print
-    html
-    mdTxt = tomd.Tomd(html).markdown
-    print
-    'markdown :{}'.format(mdTxt)
-    createFile(mdTxt)
+def convertHtml(file):
+    print('已读取：' + str(len(file.read())))
+    content =  tomd.Tomd(file.read()).markdown
+    print('已转换：' + str(len(content)))
+    file.close()
+    return content
 
-
-def createFile(mdTxt):
-
-    print('准备写入文件：{}'.format(save_file))
-    # r+ 打开一个文件用于读写。文件指针将会放在文件的开头。
-    # w+ 打开一个文件用于读写。如果该文件已存在则将其覆盖。如果该文件不存在，创建新文件。
-    # a+ 打开一个文件用于读写。如果该文件已存在，文件指针将会放在文件的结尾。文件打开时会是追加模式。如果该文件不存在，创建新文件用于读写。
-    f = codecs.open(save_file, 'w+', 'utf-8')
-    # f.write('###{}\n'.format(url))
-    f.write(mdTxt)
-    # f.write(mdTxt)
+def wrtMarkdown(file_content, file_name):
+    depath = os.path.join(save_file_path, file_name + '.md')
+    f = open(depath, 'w', encoding='utf-8')
+    f.write(file_content)
     f.close()
-    print
-    '写入文件结束：{}'.format(f.name)
+    return None
+
+if __name__ == '__main__':
+    for parent, dirnames, filenames in os.walk(load_file_path, followlinks=True):
+        for filename in filenames:
+            full_path = os.path.join(parent, filename)
+            temp_file, temp_name = getHtml(full_path)
+            wrtMarkdown(convertHtml(temp_file),temp_name)
+
+
+
 
 
